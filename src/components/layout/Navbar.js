3c0e4d0dc256bd2f4b89633d0162be2347b45880
haefneryan <haefner.ryan@gmail.com>
2022-05-23 14:45:19 -0400

    nav styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,29 @@
-import { Link } from 'react-router-dom';
-import classes from './Navbar.css'
+import { Link } from "react-router-dom";
+import classes from "./Navbar.css";
 
 function Navbar(props) {
-    const { triageOrders, dashboardOrders, completedOrders } = props
-    return (
-        <header className={classes.header}>
-            <nav>
-                <ul>
-                    {/* <li><Link to='/'>ALL ORDERS</Link></li> */}
-                    <li><Link to='/triage'>TRIAGE ({triageOrders.length})</Link></li>
-                    <li><Link to='/dashboard'>DASHBOARD ({dashboardOrders.length})</Link></li>
-                    <li><Link to='/completed'>COMPLETED ({completedOrders.length})</Link></li>
-                    <li><Link to='/create-new-order'>CREATE NEW ORDER</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const { triageOrders, dashboardOrders, completedOrders } = props;
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          {/* <li><Link to='/'>ALL ORDERS</Link></li> */}
+          <li>
+            <Link to="/triage">TRIAGE ({triageOrders.length})</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">DASHBOARD ({dashboardOrders.length})</Link>
+          </li>
+          <li>
+            <Link to="/completed">COMPLETED ({completedOrders.length})</Link>
+          </li>
+          <li>
+            <Link to="/create-new-order">CREATE NEW ORDER</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Triage.js
+++ src/pages/Triage.js
@@ -1,126 +1,185 @@
-import React, { useEffect, useState } from 'react'
-import ColumnFilters from '../components/layout/Table/ColumnFilters';
-import TriageOrderRow from '../components/TriageOrderRow';
-import TriageOrderChild from '../components/TriageOrderChild';
+import React, { useEffect, useState } from "react";
+import ColumnFilters from "../components/layout/Table/ColumnFilters";
+import TriageOrderRow from "../components/TriageOrderRow";
+import TriageOrderChild from "../components/TriageOrderChild";
 
-import { triageTableHeadersInitialState } from '../components/layout/Table/TriageTableHeaders';
-import { updateTriageComplete, deleteOrder } from '../functions/orderStatusFunctions';
-import ColumnHeaderCell from '../components/layout/Table/ColumnHeaderCell';
+import { triageTableHeadersInitialState } from "../components/layout/Table/TriageTableHeaders";
+import {
+  updateTriageComplete,
+  deleteOrder,
+} from "../functions/orderStatusFunctions";
+import ColumnHeaderCell from "../components/layout/Table/ColumnHeaderCell";
 
-import './Triage.css';
+import "./Triage.css";
 // import './AllOrders.css';
 
 function Triage(props) {
-    document.title = 'Scheduling Tool - Triage';
-    const { triageOrders, updateWorkload, updateTriageOwner, updateOwner, updateSameAs, removeChild, displayOrderChildren } = props;
-    const [filteredTriageOrders, setFilteredTriageOrders] = useState(triageOrders);
-    const [triageTableHeaders, setTriageTableHeaders] = useState(triageTableHeadersInitialState)
-    
-    const filterData = (column) => {
-        setTriageTableHeaders([...triageTableHeaders], triageTableHeaders[column.target.id].filters = column.target.value.toLowerCase())
-    }
+  document.title = "Scheduling Tool - Triage";
+  const {
+    triageOrders,
+    updateWorkload,
+    updateTriageOwner,
+    updateOwner,
+    updateSameAs,
+    removeChild,
+    displayOrderChildren,
+  } = props;
+  const [filteredTriageOrders, setFilteredTriageOrders] =
+    useState(triageOrders);
+  const [triageTableHeaders, setTriageTableHeaders] = useState(
+    triageTableHeadersInitialState
+  );
 
-    useEffect(() => {    
-        console.log('loading')
-        navigator.geolocation.getCurrentPosition(res => {
-            console.log(res)
-        })
-    }, [])
+  const filterData = (column) => {
+    setTriageTableHeaders(
+      [...triageTableHeaders],
+      (triageTableHeaders[column.target.id].filters =
+        column.target.value.toLowerCase())
+    );
+  };
 
-    useEffect(() => {
-        let filteredInfo = triageOrders;
-        triageTableHeaders.forEach(y => {
-            if (y.filterable === true && y.filters.length > 0) {
-                filteredInfo = filteredInfo.filter(x => {
-                    if (x[y.name].toLowerCase().includes(`${y.filters}`)) {
-                        return x
-                    }
-                })
-            }
-        })
-        setFilteredTriageOrders(filteredInfo)
-    }, [triageTableHeaders]);
-
-    const sortColumns = (header) => {
-        if (header.sortable === true) {
-          triageTableHeaders.forEach(value => {
-            if (value !== header) {
-              value.sortAsc = false
-              value.sortDesc = false
-            }
-          })
-    
-          // Sort Ascending
-          if (header.sortAsc === false && header.sortDesc === false) {
-            header.sortAsc = true;
-            setFilteredTriageOrders([...filteredTriageOrders.sort((a, b) => {
-              let x = a[header.name];
-              let y = b[header.name];
-        
-              if (x < y) { return -1 } else if (x > y) { return 1 } else { return 0 }
-            })])
-            // Sort Descending
-          } else if (header.sortAsc === true && header.sortDesc === false) {
-            header.sortAsc = false;
-            header.sortDesc = true;
-            setFilteredTriageOrders([...filteredTriageOrders.sort((a, b) => {
-              let x = a[header.name];
-              let y = b[header.name];
-        
-              if (x > y) { return -1 } else if (x < y) { return 1 } else { return 0 }
-            })])
-          } else {
-            header.sortAsc = false;
-            header.sortDesc = false;
-            setFilteredTriageOrders([...filteredTriageOrders.sort((a, b) => {
-              let x = a._id;
-              let y = b._id;
-        
-              if (x > y) { return 1 } else if (x < y) { return -1;} else { return 0 }
-            })])
+  useEffect(() => {
+    let filteredInfo = triageOrders;
+    triageTableHeaders.forEach((y) => {
+      if (y.filterable === true && y.filters.length > 0) {
+        filteredInfo = filteredInfo.filter((x) => {
+          if (x[y.name].toLowerCase().includes(`${y.filters}`)) {
+            return x;
           }
+        });
+      }
+    });
+    setFilteredTriageOrders(filteredInfo);
+  }, [triageTableHeaders]);
+
+  const sortColumns = (header) => {
+    if (header.sortable === true) {
+      triageTableHeaders.forEach((value) => {
+        if (value !== header) {
+          value.sortAsc = false;
+          value.sortDesc = false;
         }
+      });
+
+      // Sort Ascending
+      if (header.sortAsc === false && header.sortDesc === false) {
+        header.sortAsc = true;
+        setFilteredTriageOrders([
+          ...filteredTriageOrders.sort((a, b) => {
+            let x = a[header.name];
+            let y = b[header.name];
+
+            if (x < y) {
+              return -1;
+            } else if (x > y) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }),
+        ]);
+        // Sort Descending
+      } else if (header.sortAsc === true && header.sortDesc === false) {
+        header.sortAsc = false;
+        header.sortDesc = true;
+        setFilteredTriageOrders([
+          ...filteredTriageOrders.sort((a, b) => {
+            let x = a[header.name];
+            let y = b[header.name];
+
+            if (x > y) {
+              return -1;
+            } else if (x < y) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }),
+        ]);
+      } else {
+        header.sortAsc = false;
+        header.sortDesc = false;
+        setFilteredTriageOrders([
+          ...filteredTriageOrders.sort((a, b) => {
+            let x = a._id;
+            let y = b._id;
+
+            if (x > y) {
+              return 1;
+            } else if (x < y) {
+              return -1;
+            } else {
+              return 0;
+            }
+          }),
+        ]);
+      }
     }
-    
-    return (
-        <div>
-            <h3>TRIAGE ({filteredTriageOrders.length})</h3>
-            <table>
-                <thead>
-                    <tr>
-                        {triageTableHeaders.map(header => {
-                            return <ColumnHeaderCell sortColumns={sortColumns} header={header}/>
+  };
+
+  return (
+    <div>
+      <h3>TRIAGE ({filteredTriageOrders.length})</h3>
+      <table>
+        <thead>
+          <tr>
+            {triageTableHeaders.map((header) => {
+              return (
+                <ColumnHeaderCell sortColumns={sortColumns} header={header} />
+              );
+            })}
+          </tr>
+          <ColumnFilters
+            triageTableHeaders={triageTableHeaders}
+            filterData={filterData}
+            triageOrders={triageOrders}
+          />
+        </thead>
+        <tbody>
+          {filteredTriageOrders.map((order) => {
+            return (
+              <>
+                {order.child ? (
+                  <></>
+                ) : (
+                  <TriageOrderRow
+                    key={order._id}
+                    order={order}
+                    updateTriageOwner={updateTriageOwner}
+                    updateOwner={updateOwner}
+                    updateWorkload={updateWorkload}
+                    updateSameAs={updateSameAs}
+                    updateTriageComplete={updateTriageComplete}
+                    deleteOrder={deleteOrder}
+                    displayOrderChildren={displayOrderChildren}
+                    triageOrders={triageOrders}
+                  >
+                    {order.displaySameAsChildren ? (
+                      <>
+                        {order.sameasChildren.map((child) => {
+                          return (
+                            <TriageOrderChild
+                              order={order}
+                              child={child}
+                              updateTriageOwner={updateTriageOwner}
+                              updateOwner={updateOwner}
+                              removeChild={removeChild}
+                            />
+                          );
                         })}
-                    </tr>
-                    <ColumnFilters triageTableHeaders={triageTableHeaders} filterData={filterData} triageOrders={triageOrders} />
-                </thead>
-                <tbody>
-                    {filteredTriageOrders.map(order => {
-                        return (
-                            <>
-                                {order.child ? <></> :
-                                    <TriageOrderRow key={order._id} order={order} updateTriageOwner={updateTriageOwner} updateOwner={updateOwner} updateWorkload={updateWorkload} updateSameAs={updateSameAs} updateTriageComplete={updateTriageComplete} deleteOrder={deleteOrder} displayOrderChildren={displayOrderChildren} triageOrders={triageOrders} > 
-                               
-                                    {order.displaySameAsChildren ?
-                                        <>
-                                            {(order.sameasChildren).map(child => {
-                                                return (
-                                                    <TriageOrderChild order={order} child={child} updateTriageOwner={updateTriageOwner} updateOwner={updateOwner} removeChild={removeChild} />
-                                                )
-                                            })}
-                                        </>
-                                    :
-                                    <></>
-                                    }
-                                    </TriageOrderRow>
-                                }
-                            </>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </TriageOrderRow>
+                )}
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Triage
+export default Triage;
